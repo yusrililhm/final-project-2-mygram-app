8051ea573f6ef4c4850afc90e4bc410dc50df573
yusrililhm <yusrililham62@gmail.com>
2023-10-01 06:58:35 +0700

    [Register] Add : User register handler

--- handler/user_handler.go
+++ handler/user_handler.go
@@ -1,1 +1,38 @@
 package handler
+
+import (
+	"myGram/dto"
+	"myGram/pkg/errs"
+	"myGram/service/user_service"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user_service.UserService
+}
+
+func newUserHandler(userService user_service.UserService) userHandler {
+	return userHandler{
+		userService: userService,
+	}
+}
+
+func (uh *userHandler) Register(ctx *gin.Context) {
+	userPayload := &dto.NewUserRequest{}
+
+	if err := ctx.ShouldBindJSON(userPayload); err != nil {
+		errBindJson := errs.NewUnprocessableEntityError("invalid json body request")
+		ctx.AbortWithStatusJSON(errBindJson.Status(), errBindJson)
+		return
+	}
+
+	response, err := uh.userService.Add(userPayload)
+
+	if err != nil {
+		ctx.AbortWithStatusJSON(err.Status(), err)
+		return
+	}
+
+	ctx.JSON(response.StatusCode, response)
+}
