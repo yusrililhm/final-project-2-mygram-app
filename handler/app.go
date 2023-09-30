5673e5398625c075e4ad8cd14f173159c1898f28
yusrililhm <yusrililham62@gmail.com>
2023-10-01 06:58:02 +0700

    [Add] Add : dependency injection

--- handler/app.go
+++ handler/app.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"myGram/infra/config"
 	"myGram/infra/database"
+	"myGram/repository/user_repository/user_pg"
+	"myGram/service/user_service"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,9 +15,20 @@ func StartApplication() {
 
 	database.InitializeDatabase()
 	db := database.GetInstanceDatabaseConnection()
-	_ = db
+
+	// dependencies injection
+	userRepo := user_pg.NewUserRepository(db)
+	userService := user_service.NewUserService(userRepo)
+	userHandler := newUserHandler(userService)
 
 	app := gin.Default()
+
+	// routing
+	users := app.Group("users")
+	
+	{
+		users.POST("/register", userHandler.Register)
+	}
 
 	app.Run(":" + config.AppConfig().Port)
 }
