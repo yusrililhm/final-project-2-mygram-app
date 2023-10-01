a52d9838227d3693746f19945eebd3f4c7ebf54b
yusrililhm <yusrililham62@gmail.com>
2023-10-01 14:36:44 +0700

    [Login] Add : endpoint user login

--- handler/user_handler.go
+++ handler/user_handler.go
@@ -10,6 +10,7 @@ import (
 
 type UserHandler interface {
 	Register(ctx *gin.Context)
+	Login(ctx *gin.Context)
 }
 
 type userHandlerImpl struct {
@@ -32,6 +33,26 @@ func (uh *userHandlerImpl) Register(ctx *gin.Context) {
 	}
 
 	response, err := uh.userService.Add(userPayload)
+
+	if err != nil {
+		ctx.AbortWithStatusJSON(err.Status(), err)
+		return
+	}
+
+	ctx.JSON(response.StatusCode, response)
+}
+
+// Login implements UserHandler.
+func (uh *userHandlerImpl) Login(ctx *gin.Context) {
+	userPayload := &dto.UserRequest{}
+
+	if err := ctx.ShouldBindJSON(userPayload); err != nil {
+		errBindJson := errs.NewUnprocessableEntityError("invalid json body request " + err.Error())
+		ctx.AbortWithStatusJSON(errBindJson.Status(), errBindJson)
+		return
+	}
+
+	response, err := uh.userService.Get(userPayload)
 
 	if err != nil {
 		ctx.AbortWithStatusJSON(err.Status(), err)
