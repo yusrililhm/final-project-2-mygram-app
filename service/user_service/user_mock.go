9f496a0c3ff28c21f9a3356cf41f9cf19403a164
yusrililhm <yusrililham62@gmail.com>
2023-10-01 10:06:07 +0700

    [Mock] Add : user service mock

--- service/user_service/user_mock.go
+++ service/user_service/user_mock.go
@@ -1,1 +1,22 @@
 package user_service
+
+import (
+	"myGram/dto"
+	"myGram/pkg/errs"
+)
+
+type userServiceMock struct {
+}
+
+var (
+	Add func(userPayload *dto.NewUserRequest) (*dto.GetUserResponse, errs.Error)
+)
+
+func NewUserServiceMock() UserService {
+	return &userServiceMock{}
+}
+
+// Add implements UserService.
+func (usm *userServiceMock) Add(userPayload *dto.NewUserRequest) (*dto.GetUserResponse, errs.Error) {
+	return Add(userPayload)
+}
