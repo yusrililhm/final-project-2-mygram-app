ebba1acff62766e2fd5c05416a2231641ebd0284
yusrililhm <yusrililham62@gmail.com>
2023-10-01 07:00:38 +0700

    [User Add Service] Add : add user service

--- service/user_service/user_service.go
+++ service/user_service/user_service.go
@@ -1,1 +1,53 @@
 package user_service
+
+import (
+	"myGram/dto"
+	"myGram/entity"
+	"myGram/pkg/errs"
+	"myGram/repository/user_repository"
+	"net/http"
+)
+
+type UserService interface {
+	Add(userPayload *dto.NewUserRequest) (*dto.GetUserResponse, errs.Error)
+}
+
+type userServiceImpl struct {
+	userRepo user_repository.UserRepository
+}
+
+func NewUserService(userRepo user_repository.UserRepository) UserService {
+	return &userServiceImpl{
+		userRepo: userRepo,
+	}
+}
+
+// Add implements UserService.
+func (userService *userServiceImpl) Add(userPayload *dto.NewUserRequest) (*dto.GetUserResponse, errs.Error) {
+
+	user := &entity.User{
+		Username: userPayload.Username,
+		Email:    userPayload.Email,
+		Age:      userPayload.Age,
+		Password: userPayload.Password,
+	}
+
+	user.HashPassword()
+
+	id, err := userService.userRepo.Create(user)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &dto.GetUserResponse{
+		StatusCode: http.StatusCreated,
+		Message:    "create new order successfully",
+		Data: dto.UserResponse{
+			Id:       *id,
+			Username: user.Username,
+			Email:    user.Email,
+			Age:      user.Age,
+		},
+	}, nil
+}
