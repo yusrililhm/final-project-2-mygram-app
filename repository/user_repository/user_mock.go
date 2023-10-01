64eb4fe667dc5b2d9909f491b931a859bc12a1dd
yusrililhm <yusrililham62@gmail.com>
2023-10-01 07:49:52 +0700

    [Mock] Add : user repo mock

--- repository/user_repository/user_mock.go
+++ repository/user_repository/user_mock.go
@@ -1,1 +1,22 @@
 package user_repository
+
+import (
+	"myGram/entity"
+	"myGram/pkg/errs"
+)
+
+var (
+	Create func(userPayload *entity.User) (int, errs.Error)
+)
+
+type userRepositoryMock struct {
+}
+
+func NewUserRepositoryMock() UserRepository {
+	return &userRepositoryMock{}
+}
+
+// Create implements UserRepository.
+func (urm *userRepositoryMock) Create(userPayload *entity.User) (int, errs.Error) {
+	return Create(userPayload)
+}
