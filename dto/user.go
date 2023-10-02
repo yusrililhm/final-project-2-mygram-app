1a247ca6d655255ceabeab1cba51ce1d8a06c6fe
yusrililhm <yusrililham62@gmail.com>
2023-10-02 07:54:02 +0700

    [Age & Password] Add : validate age and password

--- dto/user.go
+++ dto/user.go
@@ -3,8 +3,8 @@ package dto
 type NewUserRequest struct {
 	Username string `json:"username" valid:"required~Username can't be empty" example:"monday"`
 	Email    string `json:"email" valid:"required~Email can't be empty, email" example:"monday.day@email.com"`
-	Age      uint   `json:"age" valid:"required~Age can't be empty" example:"21"`
-	Password string `json:"password" valid:"required~Password can't be empty" example:"secret"`
+	Age      uint   `json:"age" valid:"required~Age can't be empty, range(8|150)~Minimum age is 8" example:"21"`
+	Password string `json:"password" valid:"required~Password can't be empty, length(6|255)~Minimum password is 6 length" example:"secret"`
 }
 
 type UserRequest struct {
