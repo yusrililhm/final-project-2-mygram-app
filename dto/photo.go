b4af700b99f712af2715f4d4cbdf0bea89f54f98
yusrililhm <yusrililham62@gmail.com>
2023-10-02 14:55:36 +0700

    [New Photo] Change: new photo to new photo request

--- dto/photo.go
+++ dto/photo.go
@@ -1,6 +1,6 @@
 package dto
 
-type NewPhoto struct {
+type NewPhotoRequest struct {
 	Title    string `json:"title" valid:"required~Title can't be empty" example:"monday awesome"`
 	PhotoUrl string `json:"caption" valid:"required~Photo URL can't be empty" example:"https://www.pinterest.com/pin/807973989398829161/"`
 	Caption  string `json:"photo_url" example:"Hello I'm Monday from Weeekly, hopefully You can do this!"`
