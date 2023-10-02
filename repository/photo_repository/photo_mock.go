469836bd9353755f2d4e67bdca666c9f3f6c3d2e
yusrililhm <yusrililham62@gmail.com>
2023-10-03 05:39:53 +0700

    [Photo Mock] Add : Photo repository mock for unit test

--- repository/photo_repository/photo_mock.go
+++ repository/photo_repository/photo_mock.go
@@ -1,1 +1,23 @@
 package photo_repository
+
+import (
+	"myGram/dto"
+	"myGram/entity"
+	"myGram/pkg/errs"
+)
+
+var (
+	AddPhoto func(photoPayload *entity.Photo) (*dto.PhotoResponse, errs.Error)
+)
+
+type photoRepositoryMock struct {
+}
+
+func NewPhotoRepositoryMock() PhotoRepository {
+	return &photoRepositoryMock{}
+}
+
+// AddPhoto implements PhotoRepository.
+func (prm *photoRepositoryMock) AddPhoto(photoPayload *entity.Photo) (*dto.PhotoResponse, errs.Error) {
+	return AddPhoto(photoPayload)
+}
