317ecb1bdab5c4cc7a8fb0f98e1e58d96f65bb83
yusrililhm <yusrililham62@gmail.com>
2023-10-02 15:59:29 +0700

    [AddPhoto] Add: AddPhoto repository done

--- repository/photo_repository/photo_repository.go
+++ repository/photo_repository/photo_repository.go
@@ -1,5 +1,11 @@
 package photo_repository
 
+import (
+	"myGram/dto"
+	"myGram/entity"
+	"myGram/pkg/errs"
+)
+
 type PhotoRepository interface {
-	
+	AddPhoto(photoPayload *entity.Photo) (*dto.PhotoResponse, errs.Error)
 }
