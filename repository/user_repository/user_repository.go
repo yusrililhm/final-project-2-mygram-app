0daf72d271c413b9ca806dc43135641f66d5d5b9
yusrililhm <yusrililham62@gmail.com>
2023-10-02 09:58:56 +0700

    [Update User] Add : update user signature method

--- repository/user_repository/user_repository.go
+++ repository/user_repository/user_repository.go
@@ -8,4 +8,6 @@ import (
 type UserRepository interface {
 	Create(userPayload *entity.User) (int, errs.Error)
 	Fetch(email string) (*entity.User, errs.Error)
+	FetchById(userId int) (*entity.User, errs.Error)
+	Update(userPayload *entity.User) (errs.Error)
 }
