ddaf6fdb1fbe4b840213bad1fbdf25fd153d7ac0
yusrililhm <yusrililham62@gmail.com>
2023-10-02 14:25:55 +0700

    [New Photo] Add: caption and tags

--- dto/photo.go
+++ dto/photo.go
@@ -1,6 +1,7 @@
 package dto
 
 type NewPhoto struct {
-	Title     string
-	PhotoUrl  string
+	Title    string `json:"title" valid:"required~Title can't be empty" example:"monday awesome"`
+	PhotoUrl string `json:"caption" valid:"required~Photo URL can't be empty" example:"https://www.pinterest.com/pin/807973989398829161/"`
+	Caption  string `json:"photo_url" example:"Hello I'm Monday from Weeekly, hopefully You can do this!"`
 }
