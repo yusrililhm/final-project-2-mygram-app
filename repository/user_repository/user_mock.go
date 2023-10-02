1ded2fedaf06052f591e31502cc6c48a95898274
yusrililhm <yusrililham62@gmail.com>
2023-10-02 10:00:53 +0700

    [Mock] Add : mock user repository

--- repository/user_repository/user_mock.go
+++ repository/user_repository/user_mock.go
@@ -6,7 +6,10 @@ import (
 )
 
 var (
-	Create func(userPayload *entity.User) (int, errs.Error)
+	Create    func(userPayload *entity.User) (int, errs.Error)
+	Fetch     func(email string) (*entity.User, errs.Error)
+	Update    func(userPayload *entity.User) errs.Error
+	FetchById func(userId int) (*entity.User, errs.Error)
 )
 
 type userRepositoryMock struct {
@@ -19,4 +22,19 @@ func NewUserRepositoryMock() UserRepository {
 // Create implements UserRepository.
 func (urm *userRepositoryMock) Create(userPayload *entity.User) (int, errs.Error) {
 	return Create(userPayload)
+}
+
+// Fetch implements UserRepository.
+func (urm *userRepositoryMock) Fetch(email string) (*entity.User, errs.Error) {
+	return Fetch(email)
+}
+
+// Update implements UserRepository.
+func (urm *userRepositoryMock) Update(userPayload *entity.User) errs.Error {
+	return Update((userPayload))
+}
+
+// FetchById implements UserRepository.
+func (urm *userRepositoryMock) FetchById(userId int) (*entity.User, errs.Error) {
+	return FetchById(userId)
 }
