3f77be75cde14100a20286cbfc7dbc2272942936
yusrililhm <yusrililham62@gmail.com>
2023-10-02 07:13:55 +0700

    [Email] Add : validate email

--- dto/user.go
+++ dto/user.go
@@ -2,14 +2,14 @@ package dto
 
 type NewUserRequest struct {
 	Username string `json:"username" valid:"required~Username can't be empty" example:"monday"`
-	Email    string `json:"email" valid:"required~Email can't be empty" example:"monday.day@email.com"`
+	Email    string `json:"email" valid:"required~Email can't be empty, email" example:"monday.day@email.com"`
 	Age      uint   `json:"age" valid:"required~Age can't be empty" example:"21"`
 	Password string `json:"password" valid:"required~Password can't be empty" example:"secret"`
 }
 
 type UserRequest struct {
-	Email    string `json:"email" valid:"required~can't be empty" example:"monday.day@email.com"`
-	Password string `json:"password" valid:"required~can't be empty" example:"secret"`
+	Email    string `json:"email" valid:"required~Email can't be empty, email" example:"monday.day@email.com"`
+	Password string `json:"password" valid:"required~Password can't be empty" example:"secret"`
 }
 
 type UserResponse struct {
