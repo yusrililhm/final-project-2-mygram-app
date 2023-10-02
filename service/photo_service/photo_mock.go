a9c3ef25cd64be8445f8253a1af075cd97b60af5
yusrililhm <yusrililham62@gmail.com>
2023-10-03 05:43:29 +0700

    [Photo Mock] Add : Photo service mock for unit test

--- service/photo_service/photo_mock.go
+++ service/photo_service/photo_mock.go
@@ -1,1 +1,22 @@
 package photo_service
+
+import (
+	"myGram/dto"
+	"myGram/pkg/errs"
+)
+
+type photoServiceMock struct {
+}
+
+var (
+	AddPhoto func(userId int, photoPayload *dto.NewPhotoRequest) (*dto.GetPhotoResponse, errs.Error)
+)
+
+func NewPhotoServiceMock() PhotoService {
+	return &photoServiceMock{}
+}
+
+// AddPhoto implements PhotoService.
+func (psm *photoServiceMock) AddPhoto(userId int, photoPayload *dto.NewPhotoRequest) (*dto.GetPhotoResponse, errs.Error) {
+	return AddPhoto(userId, photoPayload)
+}
