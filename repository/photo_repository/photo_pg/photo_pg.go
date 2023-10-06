37eb93e2bd5db93c23b7c14a45e202985be89afe
yusrililhm <yusrililham62@gmail.com>
2023-10-06 09:30:40 +0700

    [Get Photo By Id & Update Photo] Add : get photo by id and update photo

--- repository/photo_repository/photo_pg/photo_pg.go
+++ repository/photo_repository/photo_pg/photo_pg.go
@@ -50,6 +50,41 @@ const (
 					p.id
 				ASC
 	`
+	getUserAndPhotosById = `
+		SELECT
+			p.id,
+			p.title,
+			p.caption,
+			p.photo_url,
+			p.user_id,
+			p.created_at,
+			p.updated_at,
+			u.email,
+			u.username
+		FROM
+			photos as p
+		LEFT JOIN
+			users AS u
+		ON
+			p.user_id = u.id
+		WHERE
+				p.id = $1
+		ORDER BY
+			p.id
+		ASC
+	`
+
+	UpdatePhotoQuery = `
+		UPDATE
+			photos
+		SET
+			title = $2,
+			caption = $3,
+			photo_url = $4,
+			updated_at = now()
+		WHERE
+			id = $1
+	`
 )
 
 func NewPhotoRepository(db *sql.DB) photo_repository.PhotoRepository {
@@ -130,4 +165,57 @@ func (photoRepo *photoRepositoryImpl) GetPhotos() ([]photo_repository.PhotoUserM
 
 	result := photo_repository.PhotoUserMapped{}
 	return result.HandleMappingPhotoWithUser(photosUser), nil
+}
+
+// GetPhotoId implements photo_repository.PhotoRepository.
+func (photoRepo *photoRepositoryImpl) GetPhotoId(photoId int) (*photo_repository.PhotoUserMapped, errs.Error) {
+
+	photoUser := photo_repository.PhotoUser{}
+
+	row := photoRepo.db.QueryRow(getUserAndPhotosById, photoId)
+	err := row.Scan(
+		&photoUser.Photo.Id,
+		&photoUser.Photo.Title,
+		&photoUser.Photo.Caption,
+		&photoUser.Photo.PhotoUrl,
+		&photoUser.Photo.UserId,
+		&photoUser.Photo.CreatedAt,
+		&photoUser.Photo.UpdatedAt,
+		&photoUser.User.Email,
+		&photoUser.User.Username,
+	)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, errs.NewNotFoundError("photo not found")
+		}
+		return nil, errs.NewInternalServerError("something went wrong")
+	}
+
+	result := photo_repository.PhotoUserMapped{}
+	return result.HandleMappingPhotoWithUserByPhotoId(photoUser), nil
+}
+
+// UpdatePhoto implements photo_repository.PhotoRepository.
+func (photoRepo *photoRepositoryImpl) UpdatePhoto(photoId int, photoPayload *entity.Photo) errs.Error {
+	tx, err := photoRepo.db.Begin()
+
+	if err != nil {
+		tx.Rollback()
+		return errs.NewInternalServerError("something went wrong")
+	}
+
+	_, err = tx.Exec(UpdatePhotoQuery, photoId, photoPayload.Title, photoPayload.Caption, photoPayload.PhotoUrl)
+
+	if err != nil {
+		tx.Rollback()
+		return errs.NewInternalServerError("something went wrong")
+	}
+
+	if err := tx.Commit(); err != nil {
+		tx.Rollback()
+		return errs.NewInternalServerError("something went wrong")
+	}
+
+	return nil
 }
