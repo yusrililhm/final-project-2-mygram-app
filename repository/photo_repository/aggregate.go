32675457d5c00cad3a331be4b1d3a15ac758bbcc
yusrililhm <yusrililham62@gmail.com>
2023-10-06 09:31:21 +0700

    [HandleMappingPhotoWithUserByPhotoId] Add: Handle Mapping Photo With User By Photo Id

--- repository/photo_repository/aggregate.go
+++ repository/photo_repository/aggregate.go
@@ -49,3 +49,19 @@ func (pum *PhotoUserMapped) HandleMappingPhotoWithUser(photoUser []PhotoUser) []
 
 	return photosUserMapped
 }
+
+func (pum *PhotoUserMapped) HandleMappingPhotoWithUserByPhotoId(photoUser PhotoUser) *PhotoUserMapped {
+	return &PhotoUserMapped{
+		Id:        photoUser.Photo.Id,
+		Title:     photoUser.Photo.Title,
+		Caption:   photoUser.Photo.Caption,
+		PhotoUrl:  photoUser.Photo.PhotoUrl,
+		UserId:    photoUser.Photo.UserId,
+		CreatedAt: photoUser.Photo.CreatedAt,
+		UpdatedAt: photoUser.Photo.UpdatedAt,
+		User: User{
+			Email:    photoUser.User.Email,
+			Username: photoUser.User.Username,
+		},
+	}
+}
