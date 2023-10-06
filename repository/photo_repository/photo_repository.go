3bb3f338ce1ac85962c2766ae65804675b3b4650
yusrililhm <yusrililham62@gmail.com>
2023-10-06 09:30:22 +0700

    [Get Photo By Id & Update Photo] Add : get photo by id and update photo

--- repository/photo_repository/photo_repository.go
+++ repository/photo_repository/photo_repository.go
@@ -9,4 +9,6 @@ import (
 type PhotoRepository interface {
 	AddPhoto(photoPayload *entity.Photo) (*dto.PhotoResponse, errs.Error)
 	GetPhotos() ([]PhotoUserMapped, errs.Error)
+	GetPhotoId(photoId int) (*PhotoUserMapped, errs.Error)
+	UpdatePhoto(photoId int, photoPayload *entity.Photo) errs.Error
 }
