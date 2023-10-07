60c7cda8410724105d5b5e4229ac68330500c288
yusrililhm <yusrililham62@gmail.com>
2023-10-07 16:39:20 +0700

    [Comment Mock] Add: add comment service mock

--- service/comment_service/comment_mock.go
+++ service/comment_service/comment_mock.go
@@ -1,1 +1,22 @@
 package comment_service
+
+import (
+	"myGram/dto"
+	"myGram/pkg/errs"
+)
+
+type commentServiceMock struct {
+}
+
+var (
+	AddComment func(userId int, commentPayload *dto.NewCommentRequest) (*dto.GetCommentResponse, errs.Error)
+)
+
+func NewCommentServiceMock() CommentService {
+	return &commentServiceMock{}
+}
+
+// AddComment implements CommentService.
+func (csm *commentServiceMock) AddComment(userId int, commentPayload *dto.NewCommentRequest) (*dto.GetCommentResponse, errs.Error) {
+	return AddComment(userId, commentPayload)
+}
