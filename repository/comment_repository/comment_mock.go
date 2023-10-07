b8f458cc080d4013f86c4cdbd43c4c188ad16d5c
yusrililhm <yusrililham62@gmail.com>
2023-10-07 16:40:50 +0700

    [Comment Mock] Add: add comment repository mock

--- repository/comment_repository/comment_mock.go
+++ repository/comment_repository/comment_mock.go
@@ -1,1 +1,23 @@
 package comment_repository
+
+import (
+	"myGram/dto"
+	"myGram/entity"
+	"myGram/pkg/errs"
+)
+
+type commentRepositoryMock struct {
+}
+
+var (
+	AddComment func(commentPayload *entity.Comment) (*dto.NewCommentResponse, errs.Error)
+)
+
+func NewCommentRepositoryMock() CommentRepository {
+	return &commentRepositoryMock{}
+}
+
+// AddComment implements CommentRepository.
+func (crm *commentRepositoryMock) AddComment(commentPayload *entity.Comment) (*dto.NewCommentResponse, errs.Error) {
+	return AddComment(commentPayload)
+}
