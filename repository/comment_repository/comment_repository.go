c3109747789f4d7a09e5a1f6b31e228c32896a9a
yusrililhm <yusrililham62@gmail.com>
2023-10-07 16:41:11 +0700

    [Add Comment] Add: add comment repository

--- repository/comment_repository/comment_repository.go
+++ repository/comment_repository/comment_repository.go
@@ -1,1 +1,11 @@
 package comment_repository
+
+import (
+	"myGram/dto"
+	"myGram/entity"
+	"myGram/pkg/errs"
+)
+
+type CommentRepository interface {
+	AddComment(commentPayload *entity.Comment) (*dto.NewCommentResponse, errs.Error)
+}
