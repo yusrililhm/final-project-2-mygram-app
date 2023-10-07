9b812ef7fff73d63c8f95c8365a49248351a38ca
yusrililhm <yusrililham62@gmail.com>
2023-10-07 16:38:45 +0700

    [Add Comment] Add: add comment handler

--- handler/comment_handler.go
+++ handler/comment_handler.go
@@ -1,1 +1,45 @@
 package handler
+
+import (
+	"myGram/dto"
+	"myGram/entity"
+	"myGram/pkg/errs"
+	"myGram/service/comment_service"
+
+	"github.com/gin-gonic/gin"
+)
+
+type CommentHandler interface {
+	AddComment(ctx *gin.Context)
+}
+
+type commentHandlerImpl struct {
+	commentService comment_service.CommentService
+}
+
+func NewCommentHandler(commentService comment_service.CommentService) CommentHandler {
+	return &commentHandlerImpl{
+		commentService: commentService,
+	}
+}
+
+// AddComment implements CommentHandler.
+func (commentHandler *commentHandlerImpl) AddComment(ctx *gin.Context) {
+	user := ctx.MustGet("userData").(entity.User)
+	commentPayload := &dto.NewCommentRequest{}
+
+	if err := ctx.ShouldBindJSON(commentPayload); err != nil {
+		errBindJson := errs.NewUnprocessableEntityError("invalid json body request")
+		ctx.AbortWithStatusJSON(errBindJson.Status(), errBindJson)
+		return
+	}
+
+	response, err := commentHandler.commentService.AddComment(user.Id, commentPayload)
+
+	if err != nil {
+		ctx.AbortWithStatusJSON(err.Status(), err)
+		return
+	}
+
+	ctx.JSON(response.StatusCode, response)
+}
