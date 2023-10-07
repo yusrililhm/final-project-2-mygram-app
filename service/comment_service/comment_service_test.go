7976a6426923b3033f63d7fbebc065fcd691da56
yusrililhm <yusrililham62@gmail.com>
2023-10-07 16:52:07 +0700

    [Unit Test] Add: add comment service unit test

--- service/comment_service/comment_service_test.go
+++ service/comment_service/comment_service_test.go
@@ -1,1 +1,125 @@
 package comment_service_test
+
+import (
+	"myGram/dto"
+	"myGram/entity"
+	"myGram/pkg/errs"
+	"myGram/repository/comment_repository"
+	"myGram/repository/photo_repository"
+	"myGram/service/comment_service"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCommentService_AddComment_InvalidBodyRequest_Fail(t *testing.T) {
+
+	commentPayload := &dto.NewCommentRequest{}
+
+	commentRepo := comment_repository.NewCommentRepositoryMock()
+	photoRepo := photo_repository.NewPhotoRepositoryMock()
+	commentService := comment_service.NewCommentService(commentRepo, photoRepo)
+
+	response, err := commentService.AddComment(1, commentPayload)
+
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+	assert.Equal(t, http.StatusBadRequest, err.Status())
+}
+
+func TestCommentService_AddComment_PhotoNotFound_Fail(t *testing.T) {
+
+	commentPayload := &dto.NewCommentRequest{
+		PhotoId: 99,
+		Message: "lorem ipsum",
+	}
+
+	commentRepo := comment_repository.NewCommentRepositoryMock()
+	photoRepo := photo_repository.NewPhotoRepositoryMock()
+	commentService := comment_service.NewCommentService(commentRepo, photoRepo)
+
+	photo_repository.GetPhotoId = func(photoId int) (*photo_repository.PhotoUserMapped, errs.Error) {
+		return nil, errs.NewNotFoundError("photo not found")
+	}
+
+	response, err := commentService.AddComment(1, commentPayload)
+
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+	assert.Equal(t, http.StatusNotFound, err.Status())
+}
+
+func TestCommentService_AddComment_GetPhotoError_Fail(t *testing.T) {
+
+	commentPayload := &dto.NewCommentRequest{
+		PhotoId: 99,
+		Message: "lorem ipsum",
+	}
+
+	commentRepo := comment_repository.NewCommentRepositoryMock()
+	photoRepo := photo_repository.NewPhotoRepositoryMock()
+	commentService := comment_service.NewCommentService(commentRepo, photoRepo)
+
+	photo_repository.GetPhotoId = func(photoId int) (*photo_repository.PhotoUserMapped, errs.Error) {
+		return nil, errs.NewInternalServerError("something went wrong")
+	}
+
+	response, err := commentService.AddComment(1, commentPayload)
+
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+	assert.Equal(t, http.StatusInternalServerError, err.Status())
+}
+
+func TestCommentService_AddComment_ServerError_Fail(t *testing.T) {
+
+	commentPayload := &dto.NewCommentRequest{
+		PhotoId: 99,
+		Message: "lorem ipsum",
+	}
+
+	commentRepo := comment_repository.NewCommentRepositoryMock()
+	photoRepo := photo_repository.NewPhotoRepositoryMock()
+	commentService := comment_service.NewCommentService(commentRepo, photoRepo)
+
+	photo_repository.GetPhotoId = func(photoId int) (*photo_repository.PhotoUserMapped, errs.Error) {
+		return &photo_repository.PhotoUserMapped{}, nil
+	}
+
+	comment_repository.AddComment = func(commentPayload *entity.Comment) (*dto.NewCommentResponse, errs.Error) {
+		return nil, errs.NewInternalServerError("something went wrong")
+	}
+
+	response, err := commentService.AddComment(1, commentPayload)
+
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+	assert.Equal(t, http.StatusInternalServerError, err.Status())
+}
+
+func TestCommentService_AddComment_Success(t *testing.T) {
+
+	commentPayload := &dto.NewCommentRequest{
+		PhotoId: 99,
+		Message: "lorem ipsum",
+	}
+
+	commentRepo := comment_repository.NewCommentRepositoryMock()
+	photoRepo := photo_repository.NewPhotoRepositoryMock()
+	commentService := comment_service.NewCommentService(commentRepo, photoRepo)
+
+	photo_repository.GetPhotoId = func(photoId int) (*photo_repository.PhotoUserMapped, errs.Error) {
+		return &photo_repository.PhotoUserMapped{}, nil
+	}
+
+	comment_repository.AddComment = func(commentPayload *entity.Comment) (*dto.NewCommentResponse, errs.Error) {
+		return &dto.NewCommentResponse{}, nil
+	}
+
+	response, err := commentService.AddComment(1, commentPayload)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, response)
+	assert.Equal(t, http.StatusCreated, response.StatusCode)
+}
