29720045cba45e3ff9fcb5cf352616d2f13c5798
yusrililhm <yusrililham62@gmail.com>
2023-10-07 16:42:03 +0700

    [dto request and response comment] Add: request and response comment

--- dto/comment.go
+++ dto/comment.go
@@ -1,5 +1,24 @@
 package dto
 
-type NewComment struct {
-	Message string
+import (
+	"time"
+)
+
+type NewCommentRequest struct {
+	PhotoId int    `json:"photo_id" example:""`
+	Message string `json:"message" valid:"required~Message can't be empty" example:"so beautifull"`
+}
+
+type NewCommentResponse struct {
+	Id        int       `json:"id"`
+	UserId    int       `json:"user_id"`
+	PhotoId   int       `json:"photo_id"`
+	Message   string    `json:"message"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type GetCommentResponse struct {
+	StatusCode int    `json:"status_code"`
+	Message    string `json:"message"`
+	Data       any    `json:"data"`
 }
