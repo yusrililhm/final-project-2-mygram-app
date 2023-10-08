73ec0983deec710d1443d942370ceed611c3a669
yusrililhm <yusrililham62@gmail.com>
2023-10-08 10:31:50 +0700

    [Comment Mock] Add: get comment by id mock

--- repository/comment_repository/comment_mock.go
+++ repository/comment_repository/comment_mock.go
@@ -10,8 +10,9 @@ type commentRepositoryMock struct {
 }
 
 var (
-	AddComment  func(commentPayload *entity.Comment) (*dto.NewCommentResponse, errs.Error)
-	GetComments func() ([]CommentUserPhotoMapped, errs.Error)
+	AddComment     func(commentPayload *entity.Comment) (*dto.NewCommentResponse, errs.Error)
+	GetComments    func() ([]CommentUserPhotoMapped, errs.Error)
+	GetCommentById func(commentId int) (*CommentUserPhotoMapped, errs.Error)
 )
 
 func NewCommentRepositoryMock() CommentRepository {
@@ -26,4 +27,9 @@ func (crm *commentRepositoryMock) AddComment(commentPayload *entity.Comment) (*d
 // GetComments implements CommentRepository.
 func (crm *commentRepositoryMock) GetComments() ([]CommentUserPhotoMapped, errs.Error) {
 	return GetComments()
+}
+
+// GetCommentById implements CommentRepository.
+func (crm *commentRepositoryMock) GetCommentById(commentId int) (*CommentUserPhotoMapped, errs.Error) {
+	return GetCommentById(commentId)
 }
