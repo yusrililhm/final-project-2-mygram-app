6e0bd54a8d2dee335f07df78654ccab832e9407a
yusrililhm <yusrililham62@gmail.com>
2023-10-09 14:00:15 +0700

    [Swagger]

--- docs/docs.go
+++ docs/docs.go
@@ -24,7 +24,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "final-project-2-mygram-app-production.up.railway.app",
+	Host:             "localhost:8080",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "MyGram App",
--- handler/app.go
+++ handler/app.go
@@ -30,7 +30,7 @@ import (
 // @contact.name GLNG-KS07 - Group 5
 // @contact.url https://github.com/yusrililhm/group-5-final-project-2-mygram-app
 
-// @host final-project-2-mygram-app-production.up.railway.app
+// @host localhost:8080
 // @BasePath /
 
 func StartApplication() {
