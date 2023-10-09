886f1597969997452b0163f24bb858fccb5d4c53
yusrililhm <yusrililham62@gmail.com>
2023-10-09 10:06:23 +0700

    Fixed Database

--- infra/database/db.go
+++ infra/database/db.go
@@ -84,7 +84,7 @@ func handleRequiredTables() {
 						id SERIAL PRIMARY KEY,
 						name VARCHAR(50) NOT NULL,
 						social_media_url TEXT NOT NULL,
-						user_id INT NOT NIL,
+						user_id INT NOT NULL,
 						created_at TIMESTAMPTZ DEFAULT now(),
 						updated_at TIMESTAMPTZ DEFAULT now(),
 						CONSTRAINT
